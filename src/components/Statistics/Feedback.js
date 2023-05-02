529c30c6cc9f2c8f192855cfb6a241c1454f2e26
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-05-02 14:07:01 +0300

    Step 2 done

--- src/components/Statistics/Feedback.js
+++ src/components/Statistics/Feedback.js
@@ -1,18 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-const Feedback = ({ onIncrement }) => (
-  <div className="Stat__feedback">
-    <h1 className="Stat__title">Please leave feedback</h1>
-    <button type="button" onClick={onIncrement}>
-      Good
-    </button>
-    <button type="button" onClick={onIncrement}>
-      Neutral
-    </button>
-    <button type="button" onClick={onIncrement}>
-      Bad
-    </button>
-  </div>
-);
+// const Feedback = ({ onIncrement }) => (
+//   <div className="Stat__feedback">
+//     <h1 className="Stat__title">Please leave feedback</h1>
+//     <button type="button" onClick={onIncrement}>
+//       Good
+//     </button>
+//     <button type="button" onClick={onIncrement}>
+//       Neutral
+//     </button>
+//     <button type="button" onClick={onIncrement}>
+//       Bad
+//     </button>
+//   </div>
+// );
 
-export default Feedback;
+// export default Feedback;
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
-import Feedback from './Feedback';
-import StatisticsControl from './StatisticsControl';
+// import Feedback from './Feedback';
+// import StatisticsControl from './StatisticsControl';
 import './Statistics.css';
 
 class Statistics extends Component {
@@ -9,19 +9,67 @@ class Statistics extends Component {
     neutral: 0,
     bad: 0,
   };
-  handleIncrement = () => {
+  handlClickGood = () => {
     this.setState(prevState => {
+      console.log(prevState);
       return {
         good: prevState.good + 1,
       };
     });
   };
+  handlClickNeutral = () => {
+    this.setState(prevState => {
+      console.log(prevState);
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
+  handlClickBad = () => {
+    this.setState(prevState => {
+      console.log(prevState);
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
   render() {
     return (
-      <>
-        <Feedback onIncrement={this.handleIncrement} />
-        <StatisticsControl />
-      </>
+      <div>
+        <div className="Stat__feedback">
+          <h1 className="Stat__title">Please leave feedback</h1>
+
+          <button type="button" onClick={this.handlClickGood}>
+            Good
+          </button>
+
+          <button type="button" onClick={this.handlClickNeutral}>
+            Neutral
+          </button>
+
+          <button type="button" onClick={this.handlClickBad}>
+            Bad
+          </button>
+        </div>
+        <div className="Statistic__wrap">
+          <h2 className="Statistic__title">Statistics</h2>
+
+          <div className="Grid">
+            <p className="Statistic__text">
+              Good: <span className="Statistic__count">{this.state.good}</span>
+            </p>
+
+            <p className="Statistic__text">
+              Neutral:{' '}
+              <span className="Statistic__count">{this.state.neutral}</span>
+            </p>
+
+            <p className="Statistic__text">
+              Bad: <span className="Statistic__count">{this.state.bad}</span>
+            </p>
+          </div>
+        </div>
+      </div>
     );
   }
 }
